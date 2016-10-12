6eaab111dd1dff2c8d45454b88b5963862d5e4fe
Stefan Heimberg <kontakt@stefanheimberg.ch>
2016-10-12 15:23:28 +0200

    umgestellt aus servlet variante

---
+++ src/main/java/ch/stefanheimberg/versionedresourcefilter/VersionHelper.java
@@ -0,0 +1,28 @@
+package ch.stefanheimberg.versionedresourcefilter;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Stefan Heimberg <kontakt@stefanheimberg.ch>
+ */
+public final class VersionHelper {
+    
+    private static final Logger LOGGER = Logger.getLogger(VersionHelper.class.getSimpleName());
+    
+    // http://java-regex-tester.appspot.com/
+    private static final String VERSION_PATTERN = "/\\d+.\\d+.\\d+/";
+    
+    public static final String stripVersionFromPath(final String versionedPath) {
+        final Pattern p = Pattern.compile(VERSION_PATTERN);
+        final Matcher m = p.matcher(versionedPath);
+        if(m.find()) {
+            LOGGER.info("-> request uri has version number. removing version");
+            return m.replaceAll("/");
+        }
+        return versionedPath;
+    }
+    
+}
---
+++ src/main/java/ch/stefanheimberg/versionedresourcefilter/VersionedResourceServlet.java
@@ -0,0 +1,85 @@
+package ch.stefanheimberg.versionedresourcefilter;
+
+import static ch.stefanheimberg.versionedresourcefilter.VersionHelper.stripVersionFromPath;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Stefan Heimberg <kontakt@stefanheimberg.ch>
+ */
+@WebServlet(urlPatterns = {"/css/*", "/js/*", "/images/*"})
+public class VersionedResourceServlet extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(VersionedResourceServlet.class.getSimpleName());
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        final String path = request.getRequestURI().substring(request.getContextPath().length());
+        final String stripedPath = stripVersionFromPath(path);
+        LOGGER.log(Level.INFO, "-> path: {0}", path);
+        LOGGER.log(Level.INFO, "-> stripedPath: {0}", stripedPath);
+
+        final ServletContext servletContext = request.getServletContext();
+        final InputStream is = servletContext.getResourceAsStream(stripedPath);
+
+        if (null == is) {
+            response.sendError(Response.Status.NOT_FOUND.getStatusCode());
+            LOGGER.log(Level.INFO, "-> resource not found!");
+            return;
+        }
+
+        response.setStatus(Response.Status.OK.getStatusCode());
+        response.setContentType(servletContext.getMimeType(stripedPath));
+
+        try (final ReadableByteChannel inputChannel = Channels.newChannel(is);
+                final WritableByteChannel outputChannel = Channels.newChannel(response.getOutputStream())) {
+            fastChannelCopy(inputChannel, outputChannel);
+        }
+    }
+
+    public static void fastChannelCopy(final ReadableByteChannel src, final WritableByteChannel dest) throws IOException {
+        // https://thomaswabner.wordpress.com/2007/10/09/fast-stream-copy-using-javanio-channels/
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+        while (src.read(buffer) != -1) {
+            // prepare the buffer to be drained
+            buffer.flip();
+            // write to the channel, may block
+            dest.write(buffer);
+            // If partial transfer, shift remainder down
+            // If buffer is empty, same as doing clear()
+            buffer.compact();
+        }
+        // EOF will leave buffer in fill state
+        buffer.flip();
+        // make sure the buffer is fully drained.
+        while (buffer.hasRemaining()) {
+            dest.write(buffer);
+        }
+    }
+
+    @Override
+    public void init(final ServletConfig servletConfig) throws ServletException {
+        LOGGER.info("initialized");
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info("destroyed");
+    }
+
+}
