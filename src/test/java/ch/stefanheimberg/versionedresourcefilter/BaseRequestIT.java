e5e96c4042a1ad637086b18de0ca81a7f3507763
Stefan Heimberg <kontakt@stefanheimberg.ch>
2016-10-12 13:24:42 +0200

    tests schlagen nun fehl bis implementierung da ist

--- src/test/java/ch/stefanheimberg/versionedresourcefilter/BaseRequestIT.java
+++ src/test/java/ch/stefanheimberg/versionedresourcefilter/BaseRequestIT.java
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.concurrent.ThreadLocalRandom;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -17,16 +18,29 @@ abstract class BaseRequestIT {
     private static final String BASE_REQUEST_URL = "http://localhost:8080/versionedresourcefilter/";
 
     protected void assert_resource_found_and_mimetypecorrect(final String resourcePath, final String expectedMimeType) throws MalformedURLException, ProtocolException, IOException {
-        final URL url = new URL(BASE_REQUEST_URL + resourcePath);
+        final String versionedResourcePath = String.format(resourcePath, get_random_version());
+        final URL url = new URL(BASE_REQUEST_URL + versionedResourcePath);
         
         final HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         
         final int responseCode = con.getResponseCode();
-        assertThat(responseCode, is(200));
+        assertThat(versionedResourcePath, responseCode, is(200));
         
         final String contentType = con.getContentType();
-        assertThat(contentType, is(expectedMimeType));
+        assertThat(versionedResourcePath, contentType, is(expectedMimeType));
+    }
+    
+    protected String get_random_version() {
+        final ThreadLocalRandom tlr = ThreadLocalRandom.current();
+        
+        return new StringBuilder()
+                .append(tlr.nextInt(100, 200))
+                .append(".")
+                .append(tlr.nextInt(0, 10))
+                .append(".")
+                .append(tlr.nextInt(0, 999))
+                .toString();
     }
 
 }
